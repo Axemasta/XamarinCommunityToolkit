028c54088b68d8f6f33abbe8e6d8ff1fffd84ebc
Axemasta <33064621+Axemasta@users.noreply.github.com>
2021-03-16 17:38:19 +0000

    Added Android Effect, Linker Boilerplate, New Image
    Added the android effect which has not been implemented. Added the linker boilerplate to prevent linking of the native effect renderers. Added a second image which is very small to contrast the very big image.

--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BackgroundAspect/BackgroundAspectEffect.android.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BackgroundAspect/BackgroundAspectEffect.android.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Xamarin.CommunityToolkit.Effects;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Effects = Xamarin.CommunityToolkit.Android.Effects;
+
+// [assembly: ExportEffect(typeof(Effects.BackgroundAspectEffect), nameof(BackgroundAspectEffect))]
+
 namespace Xamarin.CommunityToolkit.Android.Effects
 {
-	public class BackgroundAspectEffect
+	public class BackgroundAspectEffect : PlatformEffect
 	{
-		public BackgroundAspectEffect()
-		{
-		}
+
 	}
 }
--- src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BackgroundAspect/BackgroundAspectEffect.shared.cs
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Effects/BackgroundAspect/BackgroundAspectEffect.shared.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿
+using Xamarin.Forms;
 
 namespace Xamarin.CommunityToolkit.Effects
 {
@@ -9,6 +10,15 @@ namespace Xamarin.CommunityToolkit.Effects
 		public BackgroundAspectEffect()
 			: base(EffectIds.BackgroundAspect)
 		{
+			#region Required work-around to prevent linker from removing the platform-specific implementation 
+#if __IOS__
+			if (System.DateTime.Now.Ticks < 0)
+				_ = new Xamarin.CommunityToolkit.iOS.Effects.BackgroundAspectEffect();
+// #elif __ANDROID__
+// 			if (System.DateTime.Now.Ticks < 0)
+// 				_ = new Xamarin.CommunityToolkit.Android.Effects.BackgroundAspectEffect();
+#endif
+			#endregion
 		}
 	}
 }
